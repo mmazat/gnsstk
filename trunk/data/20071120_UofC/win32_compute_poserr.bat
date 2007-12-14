418cf21c7af4481d2789815c2417ceddc39e4dc8
glenn_macgougan <glenn_macgougan@913daf16-1e40-0410-aa89-be72de876231>
2007-12-14 04:09:14 +0000

    I added a test data set.

---
+++ trunk/data/20071120_UofC/win32_compute_poserr.bat
@@ -0,0 +1,8 @@
+..\..\bin\win32\computeposerr results.txt 0 2 3 4 1 51.079428117 -114.132818611 1116.585 20
+del LatError.bmp
+del LonError.bmp
+del HgtError.bmp
+del PositionError.mtx
+del 2DError.bmp
+del 3DError.bmp
+
