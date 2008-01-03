8e296f30062b8b6d263b1f8488d028c17236635d
glenn_macgougan <glenn_macgougan@913daf16-1e40-0410-aa89-be72de876231>
2008-01-03 19:55:12 +0000

    I removed the subversion repository structure David added by accident .

--- trunk/data/20071120_UofC/win32_compute_poserr.bat
+++ trunk/data/20071120_UofC/win32_compute_poserr.bat
@@ -1,4 +1,4 @@
-..\..\bin\computeposerr results.txt 0 2 3 4 1 51.079428117 -114.132818611 1116.585 20
+..\..\bin\computeposerr results.txt 0 2 3 4 1 51.079428117 -114.132818611 1116.585 2
 del LatError.bmp
 del LonError.bmp
 del HgtError.bmp
